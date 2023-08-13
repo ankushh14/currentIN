43ca94836ed5fd713ad202a3676a9ed2ea3a8437
Ankush <ankushshenoy97@gmail.com>
2023-08-13 21:16:55 +0530

    cards added

--- src/components/ShareCars/Sharecards.jsx
+++ src/components/ShareCars/Sharecards.jsx
@@ -1,6 +1,23 @@
-function Sharecards() {
+function Sharecards({name,symbol,currency,country,logo}) {
    return(
-    <>
+    <>   
+      <div className="card-div bg-sky-200 rounded-md shadow-md shadow-black flex flex-col justify-center m-6 p-5  h-[40%]">
+         <div className="card-name m-1 mx-4">
+            <h1 className="text-xl font-bold">{name}</h1>
+         </div>
+         <div className="card-symbol m-1 mx-4 font-semibold">
+            <h1>{symbol}</h1>
+         </div>
+         <div className="card-currency m-1 mx-4">
+            <h1>{currency}</h1>
+         </div>
+         <div className="card-image m-1 mx-4 flex justify-center">
+            <img src={logo} alt="logo" className="h-[20vh]" />
+         </div>
+         <div className="card-country m-1 mx-4">
+            <h1>{country}</h1>
+         </div>
+      </div>
     </>
    ) 
 }
--- src/components/Shares.jsx
+++ src/components/Shares.jsx
@@ -1,7 +1,19 @@
+import Sharecards from "./ShareCars/Sharecards";
+import logos from "../assets/stockimage.svg"
+
 function Shares(){
     return(
         <>
-            <h1>Hello SHARES</h1>
+            <div className="main-cont mt-2 flex flex-wrap justify-center m-1 lg:m-4">
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+                <Sharecards name={"Tata Consultancy Services"} symbol={"TCS"} currency={"INR"} country={"India"} logo={logos}/>
+            </div>
         </>
     )
 }
