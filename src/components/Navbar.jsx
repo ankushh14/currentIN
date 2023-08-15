7d4281b57814af20b89ed1c8c8f103c1cac277c2
Ankush <ankushshenoy97@gmail.com>
2023-08-15 19:32:48 +0530

    update again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ function Navbar() {
             <h1 className="text-2xl font-bold text-white">CurrentIN</h1>
             </Link>
             <div className="bars flex flex-col lg:hidden cursor-pointer" onClick={()=>{setNav(!Nav);}}>
-                <span className={`lines  border border-white m-1 w-6 transition-all duration-200 ${!Nav?"rotate-45 translate-y-2.5":""}`}></span>
-                <span className={`lines  border border-white m-1 w-6 transition-all duration-200 ${!Nav?"-rotate-45":""}`}></span>
+                <span className={`lines  border border-white m-1 w-6 transition-all duration-300 ${!Nav?"rotate-45 translate-y-2.5":""}`}></span>
+                <span className={`lines  border border-white m-1 w-6 transition-all duration-300 ${!Nav?"-rotate-45":""}`}></span>
                 <span className={`lines border border-white m-1 w-6 transition-all ${!Nav?"hidden -translate-y-2":""}`}></span>
             </div>
             
-            <ul className={`nav-list z-50 absolute bg-black  p-4 top-[4.5rem] flex flex-col w-screen ${!Nav?"left-0":"-left-full"} transition-all duration-200 lg:transition-none lg:static lg:flex lg:flex-row lg:w-fit lg:p-0 lg:border-none`}>
+            <ul className={`nav-list z-50 absolute bg-black  p-4 top-[4.5rem] flex flex-col w-screen ${!Nav?"left-0":"-left-full"} transition-all duration-300 lg:transition-none lg:static lg:flex lg:flex-row lg:w-fit lg:p-0 lg:border-none`}>
                 <NavLink to = "/">
                 <li className="list-items text-white m-3 lg:m-0 lg:mx-4 cursor-pointer text-center lg:hover:text-gray-300 lg:transition-colors" onClick={()=>{setNav(!Nav)}}>
                     <i className='bx bxs-home-circle'></i>Home
